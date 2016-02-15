68f06bb42271560cd48f77e7d08eea0f9cb0abd9
zxl <304924946@qq.com>
2016-02-15 16:37:13 +0800

    commit

--- web/common/test/src/main/java/proxy/CGLibProxy.java
+++ web/common/test/src/main/java/proxy/CGLibProxy.java
@@ -15,11 +15,22 @@ public class CGLibProxy implements MethodInterceptor {
 
     @Override
     public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
-        return null;
+        before();
+        Object result = methodProxy.invokeSuper(o, objects);
+        after();
+        return result;
     }
 
     public <T> T getProxy(Class<T> cls) {
         return (T) Enhancer.create(cls, this);
+    }
+
+    private void before() {
+        System.out.println("before");
+    }
+
+    private void after() {
+        System.out.println("after");
     }
 
 
