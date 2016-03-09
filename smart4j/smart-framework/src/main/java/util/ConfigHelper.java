fd3ba5b6d4c89af95752ac4a71b9310e8efdc4d9
zxl <304924946@qq.com>
2016-03-09 20:10:45 +0800

    commit

--- smart4j/smart-framework/src/main/java/util/ConfigHelper.java
+++ smart4j/smart-framework/src/main/java/util/ConfigHelper.java
@@ -1,10 +1,14 @@
 package util;
 
+import java.util.Properties;
+
 /**
  * Created by zhaoxuliang on 16/3/9.
+ * 属性文件助手
  */
 public class ConfigHelper {
 
+    private static final Properties CONFIG_PROPS = null;
 
 
 }
