459fadc2044b422446a2261f963b887d6f9740cc
zxl <304924946@qq.com>
2016-03-09 19:56:04 +0800

    commit

--- smart4j/smart-framework/src/main/java/constant/ConfigConstant.java
+++ smart4j/smart-framework/src/main/java/constant/ConfigConstant.java
@@ -2,6 +2,21 @@ package constant;
 
 /**
  * Created by zhaoxuliang on 16/3/9.
+ * 提供相关配置项常量
  */
 public class ConfigConstant {
+
+    String CONFIG_FILE = "smart.properties";
+
+    String JDBC_DRIVER = "";
+    String JDBC_URL = "";
+    String JDBC_USERNAME = "";
+    String JDBC_PASSWORD = "";
+
+    String APP_BASE_PACKAGE = "";
+    String APP_JSP_PATH = "";
+    String APP_ASSET_PATH = "";
+
+
+
 }
