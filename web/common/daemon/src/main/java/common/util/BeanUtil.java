d1680df4a49b045c2e6a10be1e97b99f0271a88b
zxl <304924946@qq.com>
2016-03-23 14:53:33 +0800

    commit

--- web/common/daemon/src/main/java/common/util/BeanUtil.java
+++ web/common/daemon/src/main/java/common/util/BeanUtil.java
@@ -1,5 +1,6 @@
 package common.util;
 
+import java.io.*;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -51,6 +52,35 @@ public class BeanUtil {
                 continue;
             }
         }
+    }
+
+    /**
+     * 深拷贝对象
+     * 被拷贝对象需继承Serializable接口
+     * @param obj
+     * @param <T>
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public static <T extends Serializable> T clone(T obj){
+        T cloneObj = null;
+        try {
+            //写入字节流
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            ObjectOutputStream obs = new ObjectOutputStream(out);
+            obs.writeObject(obj);
+            obs.close();
+
+            //分配内存，写入原始对象，生成新对象
+            ByteArrayInputStream ios = new ByteArrayInputStream(out.toByteArray());
+            ObjectInputStream ois = new ObjectInputStream(ios);
+            //返回生成的新对象
+            cloneObj = (T) ois.readObject();
+            ois.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cloneObj;
     }
 
 }
